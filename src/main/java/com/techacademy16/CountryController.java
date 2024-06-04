64ca7f668b56177ba9a23e4a9acba506a6a8107d
User0501MU <m-utsunomiya@fores.jp>
2024-06-04 12:29:09 +0900

    TutorialJPA

--- src/main/java/com/techacademy16/CountryController.java
+++ src/main/java/com/techacademy16/CountryController.java
@@ -25,4 +25,11 @@ public class CountryController {
 		return "country/list";
 	}
 
+	// 詳細画面に遷移するリンク--URLリンク式 課題追記部分
+		//リクエストマッピングのパスを一意にする: もし、同じパスを使用したい場合は、★メソッド間で異なるHTTPメソッドを使うか、クエリパラメータを使用して異なる振る舞いをするようにします。
+		@GetMapping("/detail")
+	    public String getLink(Model model) {
+	        model.addAttribute("id", "123");
+	        return "detail";//datail.html画面遷移
+		}
 }
